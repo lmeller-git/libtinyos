f086ac5b4a64ffacab5de2919afe1964296ea138
lmeller-git <louis.meller@icloud.com>
2025-07-14 20:14:52 +0200

    setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
+#![no_std]
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[panic_handler]
+fn lib_panic(info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
